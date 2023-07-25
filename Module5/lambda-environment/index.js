795b4f8ae034d203f8fae66843a6b07cded19dea
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-07-25 15:30:56 -0300

    saving

---
+++ Module5/lambda-environment/index.js
@@ -0,0 +1,14 @@
+module.exports.scheduler = async (event) => {
+  console.log("+++env:", process.env);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
